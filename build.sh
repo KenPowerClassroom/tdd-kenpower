0700c90ddd8d06bb67671a125cd3ed3e54ee42be
Ken Power <github@kenpower.com>
2023-09-15 11:50:09 +0100

    x

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+# need to change pernission to allow autograder to run this
+#git update-index --chmod=+x script.sh
 echo "Installing windows-build-tools"
 npm install --global --production windows-build-tools
 echo "Running ms build"
